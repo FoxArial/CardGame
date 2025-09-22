9712f1ad6e1f8fcc3f44aad5df5da4e9db50b6f8
Olena/Aria <savchuk.elenas@gmail.com>
2025-09-22 12:07:04 +0300

    single card logic

--- components/gameBoardScreen/SingleCard.tsx
+++ components/gameBoardScreen/SingleCard.tsx
@@ -1,44 +1,112 @@
 import { colors, width } from "@/constants/constant";
-import React, { useState } from "react";
-import {
-  Image,
-  ImageSourcePropType,
-  Pressable,
-  StyleSheet,
-  View,
-} from "react-native";
+import React, { useEffect, useRef } from "react";
+import { Animated, Image, Pressable, StyleSheet, View } from "react-native";
+import { CardType } from "./cardBoard";
 type SingleCardProps = {
-  card: { id: number; src: ImageSourcePropType };
+  card: CardType;
+  handleChoice: (card: CardType) => void;
+  disabled: boolean;
+  flipped: boolean;
+  invisibility: boolean;
 };
 const cardCover = require("@/assets/images/card-off.png");
-export default function SingleCard({ card }: SingleCardProps) {
-  const [isCardSelected, setCardSelected] = useState(false);
+export default function SingleCard({
+  card,
+  handleChoice,
+  disabled,
+  flipped,
+  invisibility,
+}: SingleCardProps) {
   const size = width * 0.12;
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+  const opacityAnim = useRef(new Animated.Value(1)).current;
+  useEffect(() => {
+    if (!invisibility) {
+      Animated.spring(rotateAnim, {
+        toValue: flipped ? 180 : 0,
+        useNativeDriver: true,
+        friction: 8,
+        tension: 10,
+      }).start();
+    }
+  }, [flipped, invisibility]);
+  useEffect(() => {
+    if (invisibility) {
+      Animated.timing(opacityAnim, {
+        toValue: 0,
+        useNativeDriver: true,
+        duration: 1500,
+      }).start();
+    }
+  }, [invisibility]);
+  const frontInterpolate = rotateAnim.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["0deg", "180deg"],
+  });
+
+  const backInterpolate = rotateAnim.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["180deg", "360deg"],
+  });
+  const handleClick = () => {
+    if (!disabled) handleChoice(card);
+  };
   return (
     <View style={styles.cardWrapper}>
       <View style={{ width: size, height: size }}>
-        <Pressable onPress={() => setCardSelected((e) => !e)}>
-          {({ pressed }) => (
-            <View
-              style={[
-                styles.cardContainer,
-                {
-                  borderColor: pressed
-                    ? colors.cardBorderSelected
-                    : colors.cardBorder,
-                },
-              ]}
-            >
-              {isCardSelected ? (
-                <Image style={styles.card} source={card.src} />
-              ) : (
+        <Pressable onPress={handleClick}>
+          <Animated.View
+            style={[
+              styles.cardContainer,
+              {
+                opacity: opacityAnim,
+              },
+            ]}
+          >
+            {flipped || invisibility ? (
+              <Animated.View
+                style={[
+                  styles.card,
+                  {
+                    transform: [{ rotateY: backInterpolate }],
+                    borderColor: colors.cardBorderSelected,
+                  },
+                ]}
+              >
                 <Image
-                  style={[styles.card, { transform: [{ scale: 1.1 }] }]}
+                  source={require("@/assets/images/purple-outline.png")}
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    height: "100%",
+                    width: "100%",
+                    zIndex: 3,
+                    transform: [{ scale: 1.2 }],
+                  }}
+                />
+                <Image style={[styles.image, {}]} source={card.src} />
+              </Animated.View>
+            ) : (
+              <Animated.View
+                style={[
+                  styles.card,
+                  {
+                    transform: [{ rotateY: frontInterpolate }],
+                    position: "absolute",
+                    borderColor: colors.cardBorder,
+                    borderWidth: 5,
+                    overflow: "hidden",
+                    borderRadius: 15,
+                  },
+                ]}
+              >
+                <Image
+                  style={[styles.image, { transform: [{ scale: 1.08 }] }]}
                   source={cardCover}
                 />
-              )}
-            </View>
-          )}
+              </Animated.View>
+            )}
+          </Animated.View>
         </Pressable>
       </View>
     </View>
@@ -51,12 +119,16 @@ const styles = StyleSheet.create({
   cardContainer: {
     width: "100%",
     height: "100%",
-    borderRadius: 10,
-    borderWidth: 3,
-    overflow: "hidden",
   },
   card: {
-    width: "101%",
-    height: "101%",
+    width: "100%",
+    height: "100%",
+    backfaceVisibility: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+
+    borderRadius: 15,
   },
 });
--- components/gameBoardScreen/cardBoard.tsx
+++ components/gameBoardScreen/cardBoard.tsx
@@ -4,20 +4,24 @@ import { ImageSourcePropType, StyleSheet, Text, View } from "react-native";
 import SingleCard from "./SingleCard";
 
 const cardImages = [
-  { src: require("@/assets/images/girl-1.png") },
-  { src: require("@/assets/images/girl-2.png") },
-  { src: require("@/assets/images/girl-3.png") },
-  { src: require("@/assets/images/girl-4.png") },
-  { src: require("@/assets/images/girl-5.png") },
-  { src: require("@/assets/images/girl-6.png") },
+  { src: require("@/assets/images/girl-1.png"), matched: false },
+  { src: require("@/assets/images/girl-2.png"), matched: false },
+  { src: require("@/assets/images/girl-3.png"), matched: false },
+  { src: require("@/assets/images/girl-4.png"), matched: false },
+  { src: require("@/assets/images/girl-5.png"), matched: false },
+  { src: require("@/assets/images/girl-6.png"), matched: false },
 ];
-
+export type CardType = {
+  id: number;
+  src: ImageSourcePropType;
+  matched: boolean;
+};
 export default function CardBoard() {
-  const [cards, setCards] = useState<
-    { id: number; src: ImageSourcePropType }[]
-  >([]);
-
-  const cardNumber = useContext(LevelContext);
+  const [cards, setCards] = useState<CardType[]>([]);
+  const [choiceOne, setChoiceOne] = useState<CardType | null>(null);
+  const [choiceTwo, setChoiceTwo] = useState<CardType | null>(null);
+  const [disabled, setDisabled] = useState(false);
+  const cardNumber = useContext(LevelContext) / 2;
   useEffect(() => {
     shuffleCards();
   }, [cardNumber]);
@@ -31,12 +35,52 @@ export default function CardBoard() {
       .map((card, index) => ({ ...card, id: index }));
     setCards(ShuffledCardImages);
   };
+
+  const handleChoice = (card: CardType) => {
+    if (disabled) return;
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setDisabled(false);
+  };
+  const updCardValue = (prevCardValue: CardType[]) => {
+    return prevCardValue.map((card) => {
+      if (card.src === choiceOne?.src) {
+        return { ...card, matched: true };
+      }
+      return card;
+    });
+  };
+  useEffect(() => {
+    if (!choiceOne || !choiceTwo) return;
+    setDisabled(true);
+    if (choiceTwo.id === choiceOne.id) {
+      setChoiceTwo(null);
+      setDisabled(false);
+      return;
+    }
+    if (choiceOne.src === choiceTwo.src) {
+      setCards((prev) => updCardValue(prev));
+      resetTurn();
+    } else {
+      setTimeout(() => resetTurn(), 1000);
+    }
+  }, [choiceOne, choiceTwo]);
   return (
     <View>
       <Text>CardBoard {cardNumber}</Text>
       <View style={styles.cardsContainer}>
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            disabled={disabled}
+            flipped={card === choiceOne || card === choiceTwo}
+            invisibility={card.matched}
+          />
         ))}
       </View>
     </View>
